b53d22ca9e11230c152e6b8d9a13f0dbeb6237ce
Jamie Czuy <jamie.czuy@gmail.com>
2017-04-12 18:19:52 -0400

    Refactor test (and include template this time)

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,26 +1,8 @@
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.test import TestCase
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_viewt(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html_V1(self):
-        request = HttpRequest()
-        response = home_page(request)
-
-        actual_html = response.content.decode('utf8')
-
-        expected_html = render_to_string('home.html')
-        self.assertEqual(expected_html, actual_html)
-
-    def test_home_page_returns_correct_html_V2(self):
+    def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
