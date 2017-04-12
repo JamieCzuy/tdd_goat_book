3a2c7986b669b934c6bb36b00af10cde459f3fb6
Jamie Czuy <jamie.czuy@gmail.com>
2017-04-11 22:25:07 -0400

    Add superlist django project and first FT for it

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -15,3 +15,9 @@ browser2.get('http://www.google.com')
 
 assert 'Google' in browser2.title
 browser2.close()  # <--- Close on Firefox browser does not work
+
+# ---- Test from the Book ---- #
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
