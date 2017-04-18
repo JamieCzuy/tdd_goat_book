bb45cec1f4034fc32007ecfc0f2d2d92b31310fa
Jamie Czuy <jamie.czuy@gmail.com>
2017-04-18 07:37:35 -0400

    Add explicit wait

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -3,14 +3,18 @@ import time
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
+
+MAX_WAIT = 10  # Seconds
 
 
 class NewVistorTest(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
-        # self.browser = webdriver.Firefox()
+        # self.browser = webdriver.Chrome()
+        self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
@@ -19,6 +23,19 @@ class NewVistorTest(LiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def _wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -45,8 +62,7 @@ class NewVistorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self._check_for_row_in_list_table('1: Buy peacock feathers')
+        self._wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -55,9 +71,8 @@ class NewVistorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        time.sleep(1)
-        self._check_for_row_in_list_table('1: Buy peacock feathers')
-        self._check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self._wait_for_row_in_list_table('1: Buy peacock feathers')
+        self._wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
