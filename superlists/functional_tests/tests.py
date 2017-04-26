b42eaa4bed181ce09d2af36f035bd722d23d951e
Jamie Czuy <jamie.czuy@gmail.com>
2017-04-26 07:59:27 -0400

    Update typo in comment

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -129,7 +129,7 @@ class NewVistorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
 
-        # SHe starts a new list and sees the input is nicely centered there to
+        # She starts a new list and sees the input is nicely centered there to
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
         self._wait_for_row_in_list_table('1: testing')
